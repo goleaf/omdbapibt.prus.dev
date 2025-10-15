aff523ddb46317e8db46ed0235ce2afea232fd43
Andrej Prus <goleaf@gmail.com>
2025-10-15 22:48:32 +0200

    feat: localize subscription access errors

--- app/Http/Middleware/EnsureSubscriptionAccess.php
+++ app/Http/Middleware/EnsureSubscriptionAccess.php
@@ -4,12 +4,17 @@ namespace App\Http\Middleware;
 
 use App\Models\User;
 use Closure;
+use Illuminate\Contracts\Translation\Translator;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class EnsureSubscriptionAccess
 {
+    public function __construct(private Translator $translator)
+    {
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -27,6 +32,9 @@ class EnsureSubscriptionAccess
             return $next($request);
         }
 
-        return redirect()->route('checkout')->with('error', trans('subscriptions.errors.premium_required'));
+        return redirect()->route('checkout')->with(
+            'error',
+            $this->translator->get('subscriptions.errors.access_required')
+        );
     }
 }
